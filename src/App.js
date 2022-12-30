2adbcd8550f8a350e08cac925755b88fbab47ffa
Vaibhavihole31 <itspinki05@gmail.com>
2022-12-30 23:41:03 +0530

    feat: fetch news

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 import './App.css'
 
 function App() {
+
+  const [data, setData] = useState("");
+
+  const getNews = () => {
+    axios.get("https://newsapi.org/v2/everything?q=twitter&from=2022-11-30&sortBy=publishedAt&apiKey=e583e41919dc42cbb7553fe611ee327b")
+
+      .then((response) => {
+        
+        // console.log(response);
+
+        setData(response.data.articles)
+      })
+  }
+
   return (
-    <div>App</div>
+    <>
+      <div>
+        <div className='conteiner my-3'>
+          <button className='btn btn-primary' onClick={getNews}>Fetch News</button>
+        </div>
+
+        <div className='container'>
+          <div className='row'>
+
+          {
+            data && data.map((value)=>{
+              return(
+                <div className='col-md-4'>
+                <div className='news-card'>
+                  <img className='news-img' src={value.urlToImage} />
+                  <h5 className='mt-3'>{value.title}</h5>
+                  <p>{value.description}</p>
+                </div>
+                </div>
+              )
+            })
+          }
+         
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
